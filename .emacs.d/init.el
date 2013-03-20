deeb2f8f0a9f62f03ab6798b7cb4405c2ed9ef80
Rovanion Luckey <rovanion.luckey@gmail.com>
2013-03-20 17:13:10 +0100

    Added parenthesis highlighting and rainbowing!

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,11 @@
+;; Each level of indention in JS is 2 spaces.
 (setq js-indent-level 2)
+
+;; Color paranthesis in all the colors of the rainbow!
+;; Requires the fallowing plugin http://www.emacswiki.org/emacs/RainbowDelimiters
+(add-to-list 'load-path "~/.emacs.d/")
+(require 'rainbow-delimiters)
+(global-rainbow-delimiters-mode)
+
+;; And then highlight the parenthesis
+(show-paren-mode)
