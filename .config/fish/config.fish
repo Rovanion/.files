088bcdae8371ab2f3d6ad1725260dbd85cd0c584
Rovanion Luckey <rovanion.luckey@gmail.com>
2013-04-18 10:50:31 +0200

    Added fish config, like .bashrc.

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+# Fix my constant mistyping.
+alias "atp-get"="apt-get"
+
+# Fix so that sudo <alias of choice> works.
+alias sudo="sudo "
+
+# Console emacs
+alias ew="emacs -nw"
+alias en="emacs -nw" 
