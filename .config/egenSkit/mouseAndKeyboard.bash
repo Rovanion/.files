50e0a0f0b2c3f42d3cbb19abc819595e360075a0
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-03-10 21:57:35 +0100

    Only run synclient stuff if there is a synaptics touchpad.

--- .config/egenSkit/mouseAndKeyboard.bash
+++ .config/egenSkit/mouseAndKeyboard.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Setting up scrolling.
-synclient HorizTwoFingerScroll=1
-synclient HorizScrollDelta=250
-synclient PalmDetect=1
-synclient VertEdgeScroll=0
-synclient TapButton2=2
-
+# Setting up scrolling on laptop
+if synclient >/dev/null; then
+  synclient HorizTwoFingerScroll=1
+  synclient HorizScrollDelta=250
+  synclient PalmDetect=1
+  synclient VertEdgeScroll=0
+  synclient TapButton2=2
+fi
 # Set the keymap and set caps to be a windows key.
 setxkbmap qq dvorak
 setxkbmap -option caps:super
