f7ccad191006ab4a7bf7467842fc1f42cf430091
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-09-16 09:58:56 +0200

    Added auto install of urxvt and terminus
    And set urxvt and firefox to be the default browser and terminal.

--- setup.bash
+++ setup.bash
@@ -11,7 +11,11 @@ if [[ ! $1 == nox ]]; then
     gsettings set org.gnome.nautilus.preferences enable-interactive-search true
 
     # Debian dependency list:
-    sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer
+    sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer xfonts-terminus rxvt-unicode
+    
+    # Select default x tools on debian
+    sudo update-alternatives --set /usr/bin/urxvt x-terminal-emulator
+    sudo update-alternatives --set /usr/bin/firefox x-www-browser
 else
     sudo apt-get install git emacs24-nox hunspell htop screen
 fi
@@ -23,7 +27,7 @@ git config --global mergetool.emerge.path $HOME/.emacs.d/emerge-for-git
 # Fancy pants git log
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue)<%an>%Creset' --abbrev-commit"
 
-# Additional setup
+# Additional setup for git
 git config --global user.name "Rovanion Luckey"
 git config --global user.email "rovanion.luckey@gmail.com"
 git config --global credential.helper cache
