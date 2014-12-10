9a1b2545b609afca99fca41e2a4379e9d9171b74
Rovanion Luckey <rovanion.luckey@gmail.com>
2014-12-10 18:13:39 +0100

    Removed company-irony config parts, it was never set up properly.

--- .emacs.d/company-conf.el
+++ .emacs.d/company-conf.el
@@ -9,27 +9,19 @@
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 3)
 
-;; Configuration for irony-mode together with company-mode
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
-
-;; (optional) adds CC special commands to `company-begin-commands' in order to
-;; trigger completion at interesting places, such as after scope operator
-;;     std::|
-(add-hook 'irony-mode-hook 'company-irony-setup-begin-commands)
-
 ;; Trigger company-mode
-(add-hook 'c++-mode-hook 'company-irony)
-(add-hook 'c-mode-hook 'company-irony)
-(add-hook 'objc-mode-hook 'company-irony)
+(add-hook 'c++-mode-hook		'company-mode)
+(add-hook 'c-mode-hook			'company-mode)
+(add-hook 'objc-mode-hook		'company-mode)
+(add-hook 'lisp-mode-hook		'company-mode)
+(add-hook 'emacs-lisp-mode	'company-mode)
+(add-hook 'go-mode-hook     'company-mode)
 
-;; replace the `completion-at-point' and `complete-symbol' bindings in
-;; irony-mode's buffers by irony-mode's function
-(defun my-irony-mode-hook ()
-  (define-key irony-mode-map [remap completion-at-point]
-    'irony-completion-at-point-async)
-  (define-key irony-mode-map [remap complete-symbol]
-    'irony-completion-at-point-async))
-(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+;; Only use company-go in go-mode
+(add-hook 'go-mode-hook (lambda ()
+                          (set (make-local-variable 'company-backends) '(company-go))
+                          (company-mode)))
+
+(require 'company-go)
 
 (provide 'company-conf)
