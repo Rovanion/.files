ae6e3a161f445b19bea3eba495a81ec8180024be
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-01-07 14:37:02 +0100

    Adjusted start placements to account for the new desktop under §.

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -21,14 +21,14 @@ function run_once(command, class, tag)
 end
 
 run_once("~/.config/egenSkit/mouseAndKeyboard.bash")
-run_once("firefox", "Firefox", tags[1][4])
+run_once("firefox", "Firefox", tags[1][2])
 run_once("volumeicon")
 run_once("nm-applet")
 run_once("xscreensaver -no-splash")
-run_once("audacious", "Audacious", tags[1][4])
+run_once("audacious", "Audacious", tags[1][5])
 run_once("emacs --daemon");
 -- run_once("evolution", "Evolution", tags[1][3])
-run_once("x-terminal-emulator -e mosh --ssh='ssh -p 9001' rovanion.se -- screen -rd weechat" , "Firefox", tags[1][1])
+run_once("sleep 5 && x-terminal-emulator -e mosh --ssh='ssh -p 9001' rovanion.se -- screen -rd weechat" , "Firefox", tags[1][2])
 run_once("sleep 1 && nice -n 19 conky")
 run_once("sleep 5 && nice -n 19 redshift")
 run_once("sleep 10 && nice -n 19 dropbox start")
