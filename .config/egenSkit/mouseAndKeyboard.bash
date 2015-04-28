05b23ff7a08041400661f0db1ad46dafd1bf5683
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-04-28 12:43:01 +0200

    Set mouse speed options for the thinkpad nipple.

--- .config/egenSkit/mouseAndKeyboard.bash
+++ .config/egenSkit/mouseAndKeyboard.bash
@@ -7,6 +7,14 @@ if synclient >/dev/null; then
   synclient PalmDetect=1
   synclient VertEdgeScroll=0
   synclient TapButton2=2
+	# Touch point scroll inertia when emulating scroll wheel.
+	xinput set-prop 13 303 5
+	# Touch point acceleration profile
+	xinput set-prop 13 251 6
+	# Touch point pointer inertia
+	xinput set-prop 13 252 0.6
+	# Touch point pointer accelation
+	xinput set-prop 13 254 3
 fi
 # Set the keymap and set caps to be a windows key.
 setxkbmap qq dvorak
