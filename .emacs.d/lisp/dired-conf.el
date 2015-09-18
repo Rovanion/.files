e81e29d9286570970217aef5ef8acfa83d1d62b7
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-09-18 17:37:28 +0200

    Added dired-mode-conf.

---
+++ .emacs.d/lisp/dired-conf.el
@@ -0,0 +1,12 @@
+;;;;
+;; Settings for dired-mode.
+;;;;
+
+;; Reuse buffer when browsing.
+(toggle-diredp-find-file-reuse-dir 1)
+
+;; Use trashcan instead of deleting directly.
+(setq delete-by-moving-to-trash t)
+
+(provide 'dired-conf)
+;;; packages.el ends here
