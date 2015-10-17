e2376d5821ade43a224b37317b3f56dad94b4905
Rovanion Luckey <rovanion.luckey@gmail.com>
2015-10-17 12:14:15 +0200

    Added go-conf.

---
+++ .emacs.d/lisp/go-conf.el
@@ -0,0 +1,9 @@
+;;; go-conf -- Personal configuration for complete anything.
+
+;;; Code:
+
+;; Format before save,
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(provide 'go-conf)
+;;; go.el ends here
