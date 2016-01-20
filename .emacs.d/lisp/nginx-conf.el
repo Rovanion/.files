ad50302c34452dc6913f27a041d8dc12d78d01c9
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-01-20 15:00:14 +0100

    Nginx-mode now uses tabs instead of spaces.

--- .emacs.d/lisp/nginx-conf.el
+++ .emacs.d/lisp/nginx-conf.el
@@ -5,5 +5,8 @@
 
 (add-to-list 'auto-mode-alist '("\\`/etc/nginx/" . nginx-mode))
 
+;; Please use tabs mr. nginx-mode.
+(setq nginx-indent-tabs-mode t)
+
 (provide 'nginx-conf)
 ;; Format before save,
