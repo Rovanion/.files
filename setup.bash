5bdf1e7a7e98d0868e0a7cf647db933687f51c07
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-04-09 14:00:50 +0200

    Correct indentation in setup and symlinker.

--- setup.bash
+++ setup.bash
@@ -4,31 +4,31 @@
 ./symlinker.bash
 
 if [[ ! $1 == nox ]]; then
-    # Install my own keymap
-    sudo cp ~/.config/egenSkit/qq /usr/share/X11/xkb/symbols/
+	# Install my own keymap
+	sudo cp ~/.config/egenSkit/qq /usr/share/X11/xkb/symbols/
 
-    # Make nautilus not search through all files when you type anything
-    gsettings set org.gnome.nautilus.preferences enable-interactive-search true
+	# Make nautilus not search through all files when you type anything
+	gsettings set org.gnome.nautilus.preferences enable-interactive-search true
 
-    # Set the gtk controls to behave like emacs
-    gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
-    gconftool-2 --type=string --set /desktop/gnome/interface/gtk_key_theme Emacs
+	# Set the gtk controls to behave like emacs
+	gsettings set org.gnome.desktop.interface gtk-key-theme "Emacs"
+	gconftool-2 --type=string --set /desktop/gnome/interface/gtk_key_theme Emacs
 
 
-    # Debian dependency list:
-    sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer xfonts-terminus rxvt-unicode python-gnomekeyring volumeicon-alsa nm-applet xscreensaver file-roller keepassx mu4e maildir-utils weechat aspell-se glances
+	# Debian dependency list:
+	sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer xfonts-terminus rxvt-unicode python-gnomekeyring volumeicon-alsa nm-applet xscreensaver file-roller keepassx mu4e maildir-utils weechat aspell-se glances
 
-    if lsb_release -i | grep -q Ubuntu; then
-	sudo apt-get install ubuntu-restricted-extras
-    else
-	sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer1.0-plugins-ugly
-    fi
+	if lsb_release -i | grep -q Ubuntu; then
+		sudo apt-get install ubuntu-restricted-extras
+	else
+		sudo apt-get install gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer1.0-plugins-ugly
+	fi
 
-    # Select default x tools on debian
-    sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
-    sudo update-alternatives --set x-www-browser /usr/bin/firefox
+	# Select default x tools on debian
+	sudo update-alternatives --set x-terminal-emulator /usr/bin/urxvt
+	sudo update-alternatives --set x-www-browser /usr/bin/firefox
 else
-    sudo apt-get install git emacs-nox htop screen maildir-utils mu4e weechat aspell-se glances
+	sudo apt-get install git emacs-nox htop screen maildir-utils mu4e weechat aspell-se glances
 fi
 
 # In order to add emerge as mergetool run:
--- symlinker.bash
+++ symlinker.bash
@@ -7,19 +7,19 @@
 currentDir=$(pwd)
 dest="$HOME"
 if [[ $1 != "" ]]; then
-    dest=$1
+	dest=$1
 fi
 echo "Creating links in $dest"
 
 for file in $(ls -a); do
-    # If the file is ., .., .git, this script or README then do nothing.
-    if [[ $file =~ ^.$|^..$|^.git$|^symlinker.bash$|^setup.bash$|^README$ ]]; then
-	continue
-    fi
+	# If the file is ., .., .git, this script or README then do nothing.
+	if [[ $file =~ ^.$|^..$|^.git$|^symlinker.bash$|^setup.bash$|^README$ ]]; then
+		continue
+	fi
 
-    if [[ -e $dest/$file ]]; then
-	echo $file already exists, moving existing to $file.old
-	mv $dest/$file $dest/$file.old
-    fi
-    ln -s $currentDir/$file $dest/$file
+	if [[ -e $dest/$file ]]; then
+		echo $file already exists, moving existing to $file.old
+		mv $dest/$file $dest/$file.old
+	fi
+	ln -s $currentDir/$file $dest/$file
 done
