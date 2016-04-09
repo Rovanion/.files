47b1acc108dcea7231ef863980807ac9edc6cc67
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-04-09 13:22:14 +0200

    TrackPoint conf - Use the property names instead of guessing at property ID's.

--- .config/egenSkit/mouseAndKeyboard.bash
+++ .config/egenSkit/mouseAndKeyboard.bash
@@ -9,14 +9,11 @@ if synclient &>/dev/null; then
   synclient TapButton2=2
 
 	deviceNumber=$(xinput list | grep TrackPoint | sed -rn 's/.*id=([0-9]+).*/\1/p')
-	# Touch point scroll inertia when emulating scroll wheel.
-	xinput set-prop $deviceNumber 303 5
-	# Touch point acceleration profile
-	xinput set-prop $deviceNumber 251 6
-	# Touch point pointer inertia
-	xinput set-prop $deviceNumber 252 0.6
-	# Touch point pointer accelation
-	xinput set-prop $deviceNumber 254 12
+	xinput set-prop $deviceNumber "Device Accel Profile" 6
+	xinput set-prop $deviceNumber "Device Accel Constant Deceleration" 2
+	xinput set-prop $deviceNumber "Device Accel Adaptive Deceleration" 1
+	xinput set-prop $deviceNumber "Device Accel Velocity Scaling" 200.0
+	xinput set-prop $deviceNumber "Evdev Wheel Emulation Inertia" 5
 fi
 # Set the keymap and set caps to be a windows key.
 setxkbmap qq dvorak
