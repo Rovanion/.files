96e61e5a25fea904cfa6ae9cb3bf22f97a9c08e8
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-04-18 13:47:42 +0200

    clojure-conf.el - Set automatic align of sexps actually true.

--- .emacs.d/lisp/clojure-conf.el
+++ .emacs.d/lisp/clojure-conf.el
@@ -3,10 +3,11 @@
 ;;; Code:
 
 ;; Vertical align of arguments in sexps
-(setq clojure-align-forms-automatically)
+(setq clojure-align-forms-automatically t)
 ;; The emacs forward-word and backward-word stop in CamelCases.
 (add-hook 'clojure-mode-hook #'subword-mode)
 
+(setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
 
 (provide 'clojure-conf)
 ;;; clojure-conf.el ends here
