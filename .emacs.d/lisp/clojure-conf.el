f1ae3f5c51e0d47a3b403aa058b498f1b8ef2ba7
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-05-01 21:11:53 +0200

    clojure-conf.el - Use company-mode when editing clojure.

--- .emacs.d/lisp/clojure-conf.el
+++ .emacs.d/lisp/clojure-conf.el
@@ -9,5 +9,13 @@
 
 (setq cider-cljs-lein-repl "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
 
+;; Use company-mode with cider.
+(add-hook 'cider-repl-mode-hook #'company-mode)
+(add-hook 'cider-mode-hook #'company-mode)
+(setq cider-completion-annotations-include-ns t)
+
+;; Use company in clojure-mode
+(add-hook 'clojure-mode-hook #'company-mode)
+
 (provide 'clojure-conf)
 ;;; clojure-conf.el ends here
