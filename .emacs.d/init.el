31dd0f25a56b79cebe35d0e483a21c2838779906
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-08-17 23:33:22 +0200

    Emacs - Split out emacs interaction driven customizations to it's own file.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,8 @@
 (require 'dired-conf)
 ;; Collection of keybinding customizations
 (require 'keybinds)
+;; Tell emacs "customizations" to write to another file.
+(setq custom-file "~/.emacs.d/lisp/custom.el")
 
 ;; Put scroll bar on the right in graphical mode, also remove toolbars.
 (defun graphical-fixes (dummy)
@@ -205,30 +207,3 @@
 ;;; init.el ends here
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(python-python-command "python3")
- '(python-shell-interpreter "python3")
- '(safe-local-variable-values
-	 (quote
-		((smart-tabs-mode)
-		 (smart-tabs-mode-enable nil)
-		 (eval progn
-					 (c-set-offset
-						(quote case-label)
-						(quote 0))
-					 (c-set-offset
-						(quote innamespace)
-						(quote 0))
-					 (c-set-offset
-						(quote inline-open)
-						(quote 0)))))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
---
+++ .emacs.d/lisp/custom.el
@@ -0,0 +1,27 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(python-python-command "python3")
+ '(python-shell-interpreter "python3")
+ '(safe-local-variable-values
+	 (quote
+		((smart-tabs-mode)
+		 (smart-tabs-mode-enable nil)
+		 (eval progn
+					 (c-set-offset
+						(quote case-label)
+						(quote 0))
+					 (c-set-offset
+						(quote innamespace)
+						(quote 0))
+					 (c-set-offset
+						(quote inline-open)
+						(quote 0)))))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
