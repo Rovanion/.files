fca09db80c120832af50e77eb8cfe677704db827
Rovanion Luckey <rovanion.luckey@gmail.com>
2016-09-21 13:09:06 +0200

    setup.bash - Added mosh.

--- setup.bash
+++ setup.bash
@@ -16,7 +16,7 @@ if [[ ! $1 == nox ]]; then
 
 
 	# Debian dependency list:
-	sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer xfonts-terminus xfonts-terminus-dos rxvt-unicode python-gnomekeyring volumeicon-alsa nm-applet xscreensaver file-roller keepassx mu4e maildir-utils weechat aspell-se glances
+	sudo apt-get install git awesome feh conky firefox emacs audacious nautilus hunspell eog redshift htop ttf-mscorefonts-installer xfonts-terminus xfonts-terminus-dos rxvt-unicode python-gnomekeyring volumeicon-alsa nm-applet xscreensaver file-roller keepassx mu4e maildir-utils weechat aspell-se glances mosh
 
 	if lsb_release -i | grep -q Ubuntu; then
 		sudo apt-get install ubuntu-restricted-extras
