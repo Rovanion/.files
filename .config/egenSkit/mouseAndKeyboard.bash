20e71108ce7d5e5c50b53febedc182c2c24f7a43
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-08-29 09:17:57 +0200

    Implemented track point customization for a Dell Latitude laptop.

--- .config/egenSkit/mouseAndKeyboard.bash
+++ .config/egenSkit/mouseAndKeyboard.bash
@@ -7,13 +7,29 @@ if synclient &>/dev/null; then
   synclient PalmDetect=1
   synclient VertEdgeScroll=0
   synclient TapButton2=2
+  synclient PalmMinWidth=8
 
-	deviceNumber=$(xinput list | grep TrackPoint | sed -rn 's/.*id=([0-9]+).*/\1/p')
-	xinput set-prop $deviceNumber "Device Accel Profile" 6
-	xinput set-prop $deviceNumber "Device Accel Constant Deceleration" 2
-	xinput set-prop $deviceNumber "Device Accel Adaptive Deceleration" 1
-	xinput set-prop $deviceNumber "Device Accel Velocity Scaling" 200.0
-	xinput set-prop $deviceNumber "Evdev Wheel Emulation Inertia" 5
+  # When on a ThinkPad
+  if xinput list | grep -E "TrackPoint"; then
+    deviceNumber=$(xinput list | grep -E "TrackPoint" | sed -rn 's/.*id=([0-9]+).*/\1/p')
+    xinput set-prop $deviceNumber "Device Accel Profile" 6
+    xinput set-prop $deviceNumber "Device Accel Constant Deceleration" 2
+    xinput set-prop $deviceNumber "Device Accel Adaptive Deceleration" 1
+    xinput set-prop $deviceNumber "Device Accel Velocity Scaling" 200.0
+    xinput set-prop $deviceNumber "Evdev Wheel Emulation Inertia" 5
+  fi
+  # When on a Dell Latitude
+  if xinput list | grep -E "DualPoint Stick"; then
+    deviceNumber=$(xinput list | grep -E "DualPoint Stick" | sed -rn 's/.*id=([0-9]+).*/\1/p')
+    xinput set-prop $deviceNumber "Device Accel Profile" 2
+    xinput set-prop $deviceNumber "Device Accel Constant Deceleration" 1.0
+    xinput set-prop $deviceNumber "Device Accel Adaptive Deceleration" 10.0
+    xinput set-prop $deviceNumber "Device Accel Velocity Scaling" 2.0
+    xinput set-prop $deviceNumber "Evdev Wheel Emulation Inertia" 30
+    xinput set-prop $deviceNumber "Coordinate Transformation Matrix" 1.000, 0.000, 0.000, 0.000, 1.000, 0.000, 0.000, 0.000, 1.200
+  fi
+
+
 fi
 # Set the keymap and set caps to be a windows key.
 setxkbmap qq dvorak
