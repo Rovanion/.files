d4910025ec80c2d83a1ce31b35e358429ddc5208
Rovanion Luckey <rovanion.luckey@gmail.com>
2017-10-28 15:22:22 +0200

    clojure-conf.el - Remove now redundant subword-mode commmand.

--- .emacs.d/lisp/clojure-conf.el
+++ .emacs.d/lisp/clojure-conf.el
@@ -4,8 +4,6 @@
 
 ;; Vertical align of arguments in sexps
 (setq clojure-align-forms-automatically t)
-;; The emacs forward-word and backward-word stop in CamelCases.
-(add-hook 'clojure-mode-hook #'subword-mode)
 
 (setq cider-cljs-lein-repl
 			"(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
