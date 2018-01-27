8d898c2acfff75801a5e8b081ba9b53f80ad3ed4
Rovanion Luckey <rovanion.luckey@gmail.com>
2018-01-27 11:01:27 +0100

    init.el - Add a function for printing lua tables seeing as that's not built in.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -25,6 +25,18 @@ local battery = battery_widget({adapter = "BAT0",
 -- Load Debian menu entries
 -- local debian = require("debian.menu")
 
+function rPrint(s, l, i) -- recursive Print (structure, limit, indent)
+   l = (l) or 100; i = i or "";-- default item limit, indent string
+   if (l<1) then print "ERROR: Item limit reached."; return l-1 end;
+   local ts = type(s);
+   if (ts ~= "table") then print (i,ts,s); return l-1 end
+   print (i,ts);           -- print "table"
+   for k,v in pairs(s) do  -- print "[KEY] VALUE"
+      l = rPrint(v, l, i.."\t["..tostring(k).."]");
+      if (l < 0) then break end
+   end
+   return l
+end
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
