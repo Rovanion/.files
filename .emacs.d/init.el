f36b3017dab093e04212d70f2d8586976d91e516
Rovanion Luckey <rovanion.luckey@gmail.com>
2018-04-27 09:47:33 +0200

    init.el - Added function rename-file-and-buffer.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -201,6 +201,19 @@
 ;; Make CamelCased subwords count as words.
 (add-hook 'prog-mode-hook #'subword-mode)
 
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+                    (set-visited-file-name new-name t t)))))))
+
 ;; Collection of keybinding customizations.
 ;; Should be the last thing requiered in init.el.
 (require 'keybinds)
