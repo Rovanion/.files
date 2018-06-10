9080a04666dc9026223b5148ead67ec87da47b58
Rovanion Luckey <rovanion.luckey@gmail.com>
2018-06-10 12:35:22 +0200

    elisp-conf.el - Building elc files seems to be causing more issues than it's worth.

--- .emacs.d/lisp/elisp-conf.el
+++ .emacs.d/lisp/elisp-conf.el
@@ -9,9 +9,9 @@
             (define-key emacs-lisp-mode-map
               "\C-x\C-e" 'pp-eval-last-sexp)
             ;; Recompile if .elc exists.
-            (add-hook (make-local-variable 'after-save-hook)
-                      (lambda ()
-                        (byte-recompile-directory default-directory)))
+            ;; (add-hook (make-local-variable 'after-save-hook)
+            ;;           (lambda ()
+            ;;             (byte-recompile-directory default-directory)))
             (define-key emacs-lisp-mode-map
               "\r" 'reindent-then-newline-and-indent)))
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
