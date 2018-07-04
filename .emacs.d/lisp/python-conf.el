7850131f6020f3abf6fdd461c73971891923b186
Rovanion Luckey <rovanion.luckey@gmail.com>
2018-07-04 18:42:13 +0200

    python-conf.el - Removed a whole bunch of commented out cruft from the file.
    If this breaks something, please fix it future me.

--- .emacs.d/lisp/python-conf.el
+++ .emacs.d/lisp/python-conf.el
@@ -1,24 +1,5 @@
 ;;; Code:
 
-;; Use Python 3 by default
-;; (setq python-remove-cwd-from-path nil)
-;; (custom-set-variables
-;;  '(python-python-command "python3")
-;;  '(python-shell-interpreter "python3"))
-;; (defun python-shell-parse-command ()
-;;   "Return the string used to execute the inferior Python process."
-;;   "/usr/bin/python3 -i")
-
-(setq jedi:environment-root "jedi-virtualenv")
-;; (setq jedi:environment-virtualenv
-;;       (list "virtualenv" "--system-site-packages" "ENV" "-p" "/usr/bin/python3"))
-
-;; Set up the python interpretern needed for company.
-;; (defun run-python-once ()
-;;   (remove-hook 'python-mode-hook 'run-python-once)
-;;   (run-python))
-;; (add-hook 'python-mode-hook 'run-python-once)
-
 (require 'elpy)
 (elpy-enable)
 
@@ -38,7 +19,6 @@
 (add-hook 'python-mode-hook #'elpy-mode)
 
 (setq python-guess-indent t)
-
 
 (provide 'python-conf)
 ;;; python-conf.el ends here
