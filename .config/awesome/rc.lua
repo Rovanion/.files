2a5a0ddd39ba7a1f5f7eb4df28c73270935204f6
Rovanion Luckey <rovanion.luckey@gmail.com>
2018-11-13 11:22:22 +0100

    awesome - Added shortcuts to start firefox, thunar, nautilus and evince.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -320,6 +320,10 @@ globalkeys = gears.table.join(
     awful.key({ modkey, "Mod1"  }, "u",          fn'||awful.screen.focus_relative(-1)',          {description = "focus the previous screen", group = "screen"}),
     -- Launching programs
     awful.key({ modkey,         }, "Return",     fn'||awful.spawn(terminal)',                    {description = "open a terminal",           group = "launcher"}),
+    awful.key({ modkey, "Shift" }, "f",          fn'||awful.spawn("firefox")',                   {description = "open firefox",              group = "launcher"}),
+    awful.key({ modkey, "Shift" }, "t",          fn'||awful.spawn("thunar")',                    {description = "open thunar",               group = "launcher"}),
+    awful.key({ modkey, "Shift" }, "n",          fn'||awful.spawn("nautilus")',                  {description = "open nautilus",             group = "launcher"}),
+    awful.key({ modkey, "Shift" }, "odiaeresis", fn'||awful.spawn("evince")',                    {description = "open evince",               group = "launcher"}),
     -- Window size
     awful.key({ modkey,         }, "a",          fn'||awful.tag.incmwfact( 0.05)',               {description = "increase main width",       group = "layout"}),
     awful.key({ modkey,         }, "e",          fn'||awful.tag.incmwfact(-0.05)',               {description = "decrease main width",       group = "layout"}),
