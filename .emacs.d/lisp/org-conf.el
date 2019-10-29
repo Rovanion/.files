9f520019f9111a439899484446a705ded617a415
Rovanion Luckey <rovanion.luckey@gmail.com>
2019-10-29 08:07:20 +0100

    emacs - Corrected styling of comments in org-conf.el

--- .emacs.d/lisp/org-conf.el
+++ .emacs.d/lisp/org-conf.el
@@ -4,13 +4,13 @@
 
 
 (require 'org)
-;; To enable the easy templates
+;; To enable the easy templates.
 (require 'org-tempo)
 (require 'ob-clojure)
 (setq org-babel-clojure-backend 'cider)
 (require 'cider)
 
-;; Active Babel languages
+;; Active Babel languages.
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((R       . t)
@@ -19,7 +19,7 @@
    (ruby    . t)
    (clojure . t)))
 
-;; Never prompt whether or not to run code
+;; Never prompt whether or not to run code.
 (setq org-confirm-babel-evaluate (lambda (lang body) nil))
 
 (provide 'org-conf)
