8291e541c15152c1f8860324b2b1924acd6d315e
Rovanion Luckey <rovanion.luckey@gmail.com>
2020-03-04 14:10:55 +0100

    emacs - Removed disused config in elisp-conf.el

--- .emacs.d/lisp/elisp-conf.el
+++ .emacs.d/lisp/elisp-conf.el
@@ -4,16 +4,7 @@
 (add-hook 'emacs-lisp-mode-hook
           (lambda ()
             ;; Use spaces, not tabs.
-            (setq indent-tabs-mode nil)
-            ;; Pretty-print eval'd expressions.
-            (define-key emacs-lisp-mode-map
-              "\C-x\C-e" 'pp-eval-last-sexp)
-            ;; Recompile if .elc exists.
-            ;; (add-hook (make-local-variable 'after-save-hook)
-            ;;           (lambda ()
-            ;;             (byte-recompile-directory default-directory)))
-            (define-key emacs-lisp-mode-map
-              "\r" 'reindent-then-newline-and-indent)))
+            (setq indent-tabs-mode nil)))
 (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
 
 (defun elisp-showdoc (f)
