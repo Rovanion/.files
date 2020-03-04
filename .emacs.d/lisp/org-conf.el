b08dfa2c7597a1b1c3f851db9714481125de6922
Rovanion Luckey <rovanion.luckey@gmail.com>
2020-03-04 11:31:14 +0100

    emacs - Added snippet for repeating the last source block in org-mode

--- .emacs.d/lisp/org-conf.el
+++ .emacs.d/lisp/org-conf.el
@@ -39,4 +39,38 @@
 ;; Enable auto-fill-mode when editing org-files.
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
+(defun org-previous-source-block-headers ()
+  "Returns the previous source block's headers."
+  (save-excursion
+    (org-babel-previous-src-block)
+    (let ((element (org-element-at-point)))
+      (when (eq (car element) 'src-block)
+        (let* ((content  (cadr element))
+               (lang     (plist-get content :language))
+               (switches (plist-get content :switches))
+               (parms    (plist-get content :parameters)))
+          (delq nil (list lang switches parms)))))))
+
+(defun org-previous-source-block-headers-string ()
+  "Returns the previous source block's headers as a string."
+  (mapconcat #'identity
+             (org-previous-source-block-headers)
+              " "))
+
+(defun org-previous-source-block-without-content ()
+  "Returns the previous source block, except its contents."
+  (format
+   (concat "#+begin_src %s\n"
+           "\n"
+           "#+end_src\n")
+   (org-previous-source-block-headers-string)))
+
+(defun org-repeat-previous-source-block-without-content ()
+  "Inserts the previous source block, except the actual code in
+the block, into the buffer."
+  (interactive)
+    (when-let (result (org-previous-source-block-without-content))
+      (insert result)
+      (previous-line 2)))
+
 (provide 'org-conf)
