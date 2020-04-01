5d9795ebfa51cd2504531b38c848530f0974d7ec
Rovanion Luckey <rovanion.luckey@gmail.com>
2020-04-01 13:53:38 +0200

    emacs - Small style fix in keybinds.el

--- .emacs.d/lisp/keybinds.el
+++ .emacs.d/lisp/keybinds.el
@@ -26,9 +26,8 @@
 
 (require 'visual-regexp)
 (define-key global-map (kbd "C-c r") 'vr/replace)
-(define-key global-map (kbd "M-%") 'vr/query-replace)
+(define-key global-map (kbd "M-%")   'vr/query-replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
-;; If you use multiple-cursors, this is for you:
 (define-key global-map (kbd "C-c m") 'vr/mc-mark)
 
 (global-set-key (kbd "<f5>") 'recompile)
