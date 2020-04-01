e1697f1e82efa0bf87a7d3569dd895e38792646a
Rovanion Luckey <rovanion.luckey@gmail.com>
2020-04-01 07:45:34 +0200

    emacs - Automatically activate nginx-mode when editing files in /etc/nginx

--- .emacs.d/lisp/nginx-conf.el
+++ .emacs.d/lisp/nginx-conf.el
@@ -4,9 +4,10 @@
 (require 'nginx-mode)
 
 (add-to-list 'auto-mode-alist '("\\`/etc/nginx/" . nginx-mode))
+(add-to-list 'auto-mode-alist '("/nginx/sites-\\(?:available\\|enabled\\)/" . nginx-mode))
 
 ;; Please use tabs mr. nginx-mode.
-(setq nginx-indent-tabs-mode t)
+(setq nginx-indent-tabs-mode nil)
 
 (provide 'nginx-conf)
 ;; Format before save,
