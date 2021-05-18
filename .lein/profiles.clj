5e404bf68d6ba4f1ba50c176ed044ffd0f95f94d
Christian Luckey <christian.luckey.external@arriver.com>
2021-05-18 09:15:12 +0200

    leiningen - Remove refactor-nrepl from repl plugins
    I don't think I've ever even used it.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,2 @@
-{:repl {:plugins [[refactor-nrepl "2.5.0"]
-                  [cider/cider-nrepl "0.25.5"]]}
+{:repl {:plugins [[cider/cider-nrepl "0.25.5"]]}
  :user {:plugins [[lein-nevam "0.1.2"]]}}
