91d3957cfd14d80a5024e5701301e3b44815051d
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-08-10 09:44:03 +0200

    emacs - Added Guix specific scheme settings
    This will be broken on all machines that I don't have the Guix source code on.

--- .emacs.d/lisp/scheme-conf.el
+++ .emacs.d/lisp/scheme-conf.el
@@ -1,8 +1,11 @@
-(add-hook 'scheme-mode-hook 'rovanion-scheme-hook)
-
 (defun rovanion-scheme-hook ()
   "Custom behaviours for `scheme-mode'."
-  (setq-local indent-tabs-mode nil))
+  (setq-local indent-tabs-mode nil)
+  (require 'geiser-guile)
+  (add-to-list 'geiser-guile-load-path "~/source/guix/main/")
+  (add-to-list 'yas-snippet-dirs       "~/source/guix/main/etc/snippets"))
+
+(add-hook 'scheme-mode-hook 'rovanion-scheme-hook)
 
 (provide 'scheme-conf)
 ;;; scheme-conf.el ends here
