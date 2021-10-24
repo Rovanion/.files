f9086fbdce97fad8089d14e69c8bd60ab0c61fef
Rovanion Luckey <rovanion.luckey@gmail.com>
2021-10-24 09:31:52 +0200

    autostart.lua - Replace Dropbox with Syncthing

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -14,5 +14,5 @@ utils.run_if_not_running("emacs", "--daemon");
 utils.run_if_not_running("urxvt", "-e mosh --ssh='ssh -p 9001' rovanion.se -- screen -rd weechat")
 utils.run_if_not_running("conky")
 utils.run_if_not_running("redshift", "-m vidmode -l 50:16")
-utils.run_if_not_running("dropbox start")
+utils.run_if_not_running("syncthing", "-no-browser")
 --}}}
