2d173240d4aa848758d810b1f3ac055b2782ea56
Rovanion Luckey <rovanion.luckey@gmail.com>
2023-02-16 18:24:58 +0100

    emacs - Add interactive functions for reloading dir-locals
    Dir-locals are directory local variables in Emacs.

--- .emacs.d/lisp/interactive-commands.el
+++ .emacs.d/lisp/interactive-commands.el
@@ -13,4 +13,33 @@
           (rename-file filename new-name t)
           (set-visited-file-name new-name t t)))))))
 
+
+;; Functions for hacking on .dir-locals.
+
+(defun reload-dir-locals-for-current-buffer ()
+  "reload dir locals for the current buffer"
+  (interactive)
+  (let ((enable-local-variables :all))
+    (hack-dir-local-variables-non-file-buffer)))
+
+(defun reload-dir-locals-for-all-buffer-in-this-directory ()
+  "For every buffer with the same `default-directory` as the
+current buffer's, reload dir-locals."
+  (interactive)
+  (let ((dir default-directory))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (equal default-directory dir)
+          (reload-dir-locals-for-current-buffer))))))
+
+
+(add-hook 'emacs-lisp-mode-hook
+          (defun enable-autoreload-for-dir-locals ()
+            (when (and (buffer-file-name)
+                       (equal dir-locals-file
+                              (file-name-nondirectory (buffer-file-name))))
+              (add-hook 'after-save-hook
+                        'reload-dir-locals-for-all-buffer-in-this-directory
+                        nil t))))
+
 (provide 'interactive-commands)
