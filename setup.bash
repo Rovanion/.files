817df52578555946c42dcc16bdd3779be1eead6c
Rovanion Luckey <rovanion.luckey@gmail.com>
2024-01-16 14:17:33 +0100

    setub.bash - Automate installation of signal-desktop

--- setup.bash
+++ setup.bash
@@ -23,6 +23,16 @@ if [[ ! $1 == nox ]]; then
 		sudo update-alternatives --set x-www-browser /usr/bin/firefox-esr
 	fi
 
+	## Install Signal
+	# 1. Install Signal's official public software signing key:
+	wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg
+	cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null
+	# 2. Add Signal's repository to your list of repositories:
+	echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\
+  sudo tee /etc/apt/sources.list.d/signal-xenial.list
+	# 3. Update your package database and install Signal:
+	sudo apt update && sudo apt install signal-desktop
+
 	# Install my own keymap
 	sudo cp "$gitroot/.config/xkb/symbols/qq" /usr/share/X11/xkb/symbols/
 
