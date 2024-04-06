f6aeb64a29de60b01c680f099c3b990307ab09b4
Rovanion Luckey <rovanion.luckey@gmail.com>
2024-04-06 21:35:39 +0200

    guix - Add explicit substitutes mention in channels.scm
    Honestly, I don't remember why this would be an improvement. The detection of a substitution server should be automatic but whatever.

--- .config/guix/channels.scm
+++ .config/guix/channels.scm
@@ -1,4 +1,9 @@
-(cons* (channel
+(use-modules (guix ci))
+
+(list (channel-with-substitutes-available
+              %default-guix-channel
+              "https://ci.guix.gnu.org")
+       (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         ;; Enable signature verification:
@@ -6,5 +11,4 @@
          (make-channel-introduction
           "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
           (openpgp-fingerprint
-           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-       %default-channels)
+           "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
