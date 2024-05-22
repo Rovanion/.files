9ec5ff0b4c5b9a4496602f28e81a74457c26baf6
Rovanion Luckey <rovanion.luckey@gmail.com>
2024-05-22 11:02:35 +0200

    autostart.lua - Add missing - to --no-browser argument

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -14,7 +14,7 @@ utils.run_if_not_running("emacs", "--daemon");
 utils.run_if_not_running("urxvt", "-e mosh --ssh='ssh -p 9001' rovanion.se -- screen -rd weechat")
 utils.run_if_not_running("conky")
 utils.run_if_not_running("redshift", "-m vidmode -l 50:16")
-utils.run_if_not_running("syncthing", "-no-browser")
+utils.run_if_not_running("syncthing", "--no-browser")
 utils.run_if_not_running("signal-desktop")
 utils.run_if_not_running("spotify")
 utils.run_if_not_running("urxvt -name music-main -e pulsemixer") -- Always starts due to impl. detail in run_if_not_running.
