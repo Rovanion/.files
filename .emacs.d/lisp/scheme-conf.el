de0d778fd5cb7caaabac37d3c391e00b9257fe24
Rovanion Luckey <rovanion.luckey@gmail.com>
2024-09-07 22:21:24 +0200

    scheme-conf.el - Only add guix repo paths if they exist

--- .emacs.d/lisp/scheme-conf.el
+++ .emacs.d/lisp/scheme-conf.el
@@ -2,8 +2,9 @@
   "Custom behaviours for `scheme-mode'."
   (setq-local indent-tabs-mode nil)
   (require 'geiser-guile)
-  (add-to-list 'geiser-guile-load-path "~/source/guix/main/")
-  (add-to-list 'yas-snippet-dirs       "~/source/guix/main/etc/snippets"))
+  (when (file-exists-p "~/source/guix/main/")
+    (add-to-list 'geiser-guile-load-path "~/source/guix/main/")
+    (add-to-list 'yas-snippet-dirs       "~/source/guix/main/etc/snippets")))
 
 (add-hook 'scheme-mode-hook 'rovanion-scheme-hook)
 
