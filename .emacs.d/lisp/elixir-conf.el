02bce2f1bc785b17c207da5ddc5c37386448fd7a
Rovanion Luckey <rovanion.luckey@gmail.com>
2024-09-12 10:52:05 +0200

    elixir-conf.el - Require eglot before using it

--- .emacs.d/lisp/elixir-conf.el
+++ .emacs.d/lisp/elixir-conf.el
@@ -1,6 +1,8 @@
 (with-eval-after-load 'elixir-mode
   (eglot-ensure))
 
+(require 'eglot)
+
 (add-to-list 'eglot-server-programs '(elixir-mode "~/.local/lib/elixir-ls/language_server.sh"))
 
 (provide 'elixir-conf)
