52ea2373e1e8206b056d2998111b7b07f898ec10
Rovanion Luckey <rovanion.luckey@gmail.com>
2026-04-01 19:11:18 +0200

    emacs - Add Python indentation detection

--- .emacs.d/lisp/python-conf.el
+++ .emacs.d/lisp/python-conf.el
@@ -47,10 +47,36 @@ up."
 (with-eval-after-load 'python-mode
   (eglot-ensure))
 
-;; Python mode hardcodes tab-width to 8 spaces, for some ungodly reason.
+;;; Python mode hardcodes tab-width to 8 spaces, for some ungodly reason.
+;;; Try to detect indentation depth instead.
+
+(defun how-many-region (begin end regexp &optional interactive)
+    "Print number of non-trivial matches for REGEXP in region.
+   Non-interactive arguments are Begin End Regexp"
+    (interactive "r\nsHow many matches for (regexp): \np")
+    (let ((count 0) opoint)
+      (save-excursion
+        (setq end (or end (point-max)))
+        (goto-char (or begin (point)))
+        (while (and (< (setq opoint (point)) end)
+                    (re-search-forward regexp end t))
+          (if (= opoint (point))
+              (forward-char 1)
+            (setq count (1+ count))))
+        (if interactive (message "%d occurrences" count))
+        count)))
+
+(defun infer-indentation-style ()
+  ;; If our source file uses tabs, we use tabs, if spaces spaces, and if
+  ;; neither, we use the current indent-tabs-mode.
+  (let ((space-count (how-many-region (point-min) (point-max) "^  "))
+        (tab-count (how-many-region (point-min) (point-max) "^\t")))
+    (if (> space-count tab-count) (setq indent-tabs-mode nil))
+        (if (> tab-count space-count) (setq indent-tabs-mode t))))
+
 (defun set-tab-width ()
-  (setq-local tab-width 2)
-  (setq-local python-indent-offset tab-width))
+  (setq-local indent-tabs-mode nil)
+  (infer-indentation-style))
 
 (add-hook 'python-mode-hook #'set-tab-width)
 
